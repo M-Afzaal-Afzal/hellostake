48f535fedc7706e7e63224eaa78d0b89d8ee0354
M Afzaal Afzal <afzaal1721@gmail.com>
2021-04-17 14:15:39 +0500

    basic structure completed,

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import {Chakra} from "../src/theme/Chakra";
+import Layout from '../src/components/Layout/Layout';
+import 'focus-visible/dist/focus-visible'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}) {
+
+    return (
+        <Chakra cookies={pageProps.cookies}>
+            <Layout>
+                <Component {...pageProps} />
+             </Layout>
+         </Chakra>
+    )
 }
 
 export default MyApp
