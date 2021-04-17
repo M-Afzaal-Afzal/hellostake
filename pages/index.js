943f4a8baf2677aea2bcb2ce75e571291eccf9b0
M Afzaal Afzal <afzaal1721@gmail.com>
2021-04-17 16:23:28 +0500

    redux toolkit added

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,31 @@
 import {Box, Button} from "@chakra-ui/react";
 import {DarkModeSwitch} from "../src/components/Buttons/DarkModeSwitch";
+import {useDispatch, useSelector} from "react-redux";
+import {decrement, increment,selectCounterVale} from "../src/store/counter/counterSlice";
 
 const Home = () => {
+
+    const dispatch = useDispatch();
+
+    const value = useSelector(selectCounterVale);
+    console.log(value);
+
+    const incrementHandler = () => {
+        dispatch(increment());
+    }
+
+    const decrementHandler = () => {
+        dispatch(decrement());
+    }
+
     return (
         <Box>
-            <Button colorScheme={'blue'}>testing</Button>
+            <Button onClick={incrementHandler} colorScheme={'blue'}>Increment</Button>
+            <Button onClick={decrementHandler} colorScheme={'blue'}>Decrement</Button>
+
+
             main page
+            <Box>{value}</Box>
             <Box>
                 <DarkModeSwitch/>
             </Box>
